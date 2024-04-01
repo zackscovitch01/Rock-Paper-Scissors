78032be7e970206bf0962cb39795e5518c99b0b9
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-03-31 22:57:24 -0400

    .

---
+++ app.js
@@ -0,0 +1,31 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+
+      case "paper":
+        result = computerChoice === "rock" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+
+      case "scissors":
+        result = computerChoice === "paper" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+    }
+  }
+
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+  resultDisplay.textContent = result;
+}
